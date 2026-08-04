f95c2d868ec355e5a4ff18654e3fb6e51bcd16f1
alexeyyrl-coder <alexeyyrl@email.com>
2026-08-05 00:58:49 +0200

    add all good thing

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,9 +16,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	/* Vérifier si malloc a échoué */
 	if (node == NULL)
-        {
+	{
 		return (NULL);
-        }
+	}
 	/* Initialiser n, parent, left et right */
 	node->n = value;
 	node->parent = parent;
